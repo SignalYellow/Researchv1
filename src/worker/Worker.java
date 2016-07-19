8972ea7cdc2a71d9c625ff8fac69ce1218b5ecb2
shohei <room_shoheiyamamoto@yahoo.co.jp>
2016-07-19 10:49:23 +0900

    wantsToQuit

--- src/worker/Worker.java
+++ src/worker/Worker.java
@@ -2,8 +2,22 @@ package worker;
 
 public class Worker {
 	
+	private int turn = 0;
+	
 	public Worker(){
 		
 	}
-
+	
+	public boolean goNextTurnAndWantsToQuit(){
+		if(this.wantsToQuit()){
+			return false;
+		}
+		
+		return false;
+	}
+	
+	private boolean wantsToQuit(){
+		return true;
+	}
+	
 }
